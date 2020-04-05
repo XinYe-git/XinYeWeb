3084746ddcb53576d0b1cd9b2bfdc35d360d49c9
test <test.com>
2020-04-05 18:17:07 +0800

    main已经完成

--- src/components/header/regButton/index.jsx
+++ src/components/header/regButton/index.jsx
@@ -2,10 +2,22 @@ import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 import './index.css'
 export default class reg extends Component {
+    constructor(){
+        super()
+        this.signIn={}
+    }
+    setSignIn(){
+        var to=""
+        window.location.pathname==="/"?
+        to="/main/signIn"
+        :
+        to=window.location.pathname+"/signIn"
+        return to
+    }
     render() {
         return (
             <div className='ref-button'>
-                <Link className="ref-item" to={window.location.pathname+'/signIn'}>登录</Link>
+                <Link className="ref-item" to={this.setSignIn()}>登录</Link>
                 <Link className="ref-item" to='/register' target='_blank'>注册</Link>
             </div>
         )
--- src/components/main/appreciation/index.jsx
+++ src/components/main/appreciation/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './index.css'
 import AppreciationItem from './appreciationItem'
+import More from './more'
 export default class appreciation extends Component {
     render() {
         return (
@@ -8,6 +9,7 @@ export default class appreciation extends Component {
                 <p className='main-title-1'>实例欣赏</p>
                 <p className='main-title-2'>原创的设计，高端的技术服务</p>
                 <AppreciationItem />
+                <More />
             </div>
         )
     }
