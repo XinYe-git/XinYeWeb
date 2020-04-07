91c01da370f9e5d9aa6e970686ac71edc5facaa4
test <test.com>
2020-04-08 00:27:53 +0800

    introduction已完成

--- src/components/visit/index.jsx
+++ src/components/visit/index.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import {Route,Redirect,Switch} from 'react-router-dom'
 import News from "./news"
+import Introduction from './introduction'
 export default class visit extends Component {
     render() {
         return (
             <div className="visit">
                 <Switch>
                     <Route path='/visit/news' component={News}/>
-                    <Route path='/visit/introduction' />
+                    <Route path='/visit/introduction' component={Introduction}/>
                     <Route path='/visit/join' />
                     <Redirect exact from="/visit" to="/visit/news"/>
                 </Switch>
