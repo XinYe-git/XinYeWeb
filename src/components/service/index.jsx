05e08659335523a692efcc1db4ac1e267a59069e
MuNitCat <2444393943@qq.com>
2020-04-13 23:51:20 +0800

    反馈表单数据的提交还没处理

--- src/components/service/index.jsx
+++ src/components/service/index.jsx
@@ -25,6 +25,23 @@ export default class service extends Component {
                         FEEDBACK
                         <p className="superFont">问题反馈</p>
                     </h1>
+                    <div className='phoneCity'>
+                        <form action="" method="POST" className="submitBox clearfix">
+                            <div className='conPhone'>
+                                <input type="text" className='conMod ' />
+                                <span className="conPhoneSpan">电话：</span>
+                            </div>
+                            <div className='conCity'>
+                                <input type="text" className='conMod ' />
+                                <span className="conPhoneSpan">城市：</span>
+                            </div>
+                            <div className='feedTextF'>
+                                <span className="feedPrompt">反馈：</span>
+                                <textarea type="text" className='feedBackText'></textarea>
+                            </div>
+                            <input type='submit' value='提交' className='subBoxBtn'/>
+                        </form>
+                    </div>
                 </div>
             </div>
         )
